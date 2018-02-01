decde829542200e70cd64f65aa079441c47d7140
Baz2013 <chuan.gu6@gmail.com>
2018-02-01 11:23:48 +0800

    从时间服务器获取时间demo

--- src/daytime_tcp_cli.py
+++ src/daytime_tcp_cli.py
@@ -1,1 +1,18 @@
 # -*- coding:utf-8 -*-
+# 给时间服务器发送请求获取时间
+
+import socket
+import time
+
+dest_ip = '129.6.15.28'
+dest_port = 13
+addr = (dest_ip, dest_port)
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+
+sock.connect(addr)
+sock.sendall('')
+
+data = sock.recv(64)
+
+print data
